a1c92aabb0205bab832e3d79a6f3b05e1618e9ad
treere <tomasiandrea.at@gmail.com>
2024-09-26 23:30:43 +0200

    Fix filtering

--- src/lib/names.ts
+++ src/lib/names.ts
@@ -26,22 +26,32 @@ export const filteredNamesList = (filters: Filters): Name[] => {
   if (filters.sex) n = n.filter((name) => name.sex === filters.sex);
 
   if (filters.contain.length !== 0)
-    n = n.filter((name) => filters.contain.split(',').some((v) => name.name.includes(v)));
+    n = n.filter((name) =>
+      filters.contain.split(',').some((v) => name.name.toLowerCase().includes(v))
+    );
 
   if (filters.notContain.length !== 0)
-    n = n.filter((name) => filters.contain.split(',').every((v) => !name.name.includes(v)));
+    n = n.filter(
+      (name) => !filters.notContain.split(',').some((v) => name.name.toLowerCase().includes(v))
+    );
 
   if (filters.start.length !== 0)
-    n = n.filter((name) => filters.start.split(',').some((v) => name.name.startsWith(v)));
+    n = n.filter((name) =>
+      filters.start.split(',').some((v) => name.name.toLowerCase().startsWith(v))
+    );
 
   if (filters.notStart.length !== 0)
-    n = n.filter((name) => filters.start.split(',').every((v) => !name.name.startsWith(v)));
+    n = n.filter(
+      (name) => !filters.notStart.split(',').some((v) => name.name.toLowerCase().startsWith(v))
+    );
 
   if (filters.end.length !== 0)
-    n = n.filter((name) => filters.end.split(',').some((v) => name.name.endsWith(v)));
+    n = n.filter((name) => filters.end.split(',').some((v) => name.name.toLowerCase().endsWith(v)));
 
   if (filters.notEnd.length !== 0)
-    n = n.filter((name) => filters.end.split(',').every((v) => !name.name.endsWith(v)));
+    n = n.filter(
+      (name) => !filters.notEnd.split(',').some((v) => name.name.toLowerCase().endsWith(v))
+    );
 
   return n;
 };
