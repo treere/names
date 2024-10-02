508a5cd49336c889edb171902aa060a2d0bc091e
treere <tomasiandrea.at@gmail.com>
2024-10-02 21:47:27 +0200

    TextArea in svelte5

--- src/lib/components/ui/textarea/textarea.svelte
+++ src/lib/components/ui/textarea/textarea.svelte
@@ -2,15 +2,12 @@
   import type { HTMLTextareaAttributes } from 'svelte/elements';
   import { cn } from '$lib/utils.js';
 
-  type $$Props = HTMLTextareaAttributes;
-
-  let className: $$Props['class'] = undefined;
-  export let value: $$Props['value'] = undefined;
-  export { className as class };
-
-  // Workaround for https://github.com/sveltejs/svelte/issues/9305
-  // Fixed in Svelte 5, but not backported to 4.x.
-  export let readonly: $$Props['readonly'] = undefined;
+  let {
+    class: className,
+    value = $bindable(),
+    readonly,
+    ...rest
+  }: HTMLTextareaAttributes = $props();
 </script>
 
 <textarea
@@ -20,17 +17,5 @@
   )}
   bind:value
   {readonly}
-  on:blur
-  on:change
-  on:click
-  on:focus
-  on:keydown
-  on:keypress
-  on:keyup
-  on:mouseover
-  on:mouseenter
-  on:mouseleave
-  on:paste
-  on:input
-  {...$$restProps}
+  {...rest}
 ></textarea>
