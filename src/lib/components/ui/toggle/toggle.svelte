ddb347d9e4cee02c6b23790c48de034b377ec7e0
treere <tomasiandrea.at@gmail.com>
2024-10-02 21:44:05 +0200

    Toggle in svelte5

--- src/lib/components/ui/toggle/toggle.svelte
+++ src/lib/components/ui/toggle/toggle.svelte
@@ -2,24 +2,29 @@
   import { Toggle as TogglePrimitive } from 'bits-ui';
   import { type Size, type Variant, toggleVariants } from './index.js';
   import { cn } from '$lib/utils.js';
+  import type { Snippet } from 'svelte';
 
   type $$Props = TogglePrimitive.Props & {
     variant?: Variant;
     size?: Size;
+    children: Snippet;
   };
 
-  let className: $$Props['class'] = undefined;
-  export let variant: $$Props['variant'] = 'default';
-  export let size: $$Props['size'] = 'default';
-  export let pressed: $$Props['pressed'] = undefined;
-  export { className as class };
+  let {
+    class: className,
+    variant = 'default',
+    size = 'default',
+    pressed,
+    children,
+    ...rest
+  }: $$Props = $props();
 </script>
 
 <TogglePrimitive.Root
   bind:pressed
   class={cn(toggleVariants({ variant, size, className }))}
-  {...$$restProps}
+  {...rest}
   on:click
 >
-  <slot />
+  {@render children()}
 </TogglePrimitive.Root>
