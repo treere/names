89b0a2e218a5f5d191379a99c3c2993cc1a3d822
treere <tomasiandrea.at@gmail.com>
2025-04-18 22:02:13 +0200

    Fix names page refresh

--- src/routes/names/[name]/+page.ts
+++ src/routes/names/[name]/+page.ts
@@ -1,9 +1,9 @@
+import type { EntryGenerator } from './$types';
 import M_NAMES from '$lib/MALE.list';
 import F_NAMES from '$lib/FEMALE.list';
 
-
-export function entries() {
-	return [...M_NAMES, ...F_NAMES].map((name) => ({name}));
-}
-
 export const prerender = true;
+
+export const entries: EntryGenerator = () => {
+  return [...M_NAMES, ...F_NAMES].map((name: string) => ({ name: name.toLowerCase() }));
+};
