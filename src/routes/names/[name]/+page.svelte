370acc643911e3b91f003c36f5870b5a7342ac18
treere <tomasiandrea.at@gmail.com>
2025-04-18 22:22:31 +0200

    Removed names path

--- src/routes/names/[name]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { page } from '$app/state';
-  
-  const { gender, name } = page.params;
-</script>
-
-<h1 class="text-2xl capitalize">{name} {gender}</h1>
--- src/routes/names/[name]/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { EntryGenerator } from './$types';
-import M_NAMES from '$lib/MALE.list';
-import F_NAMES from '$lib/FEMALE.list';
-
-export const prerender = true;
-
-export const entries: EntryGenerator = () => {
-  return [...M_NAMES, ...F_NAMES].map((name: string) => ({ name: name.toLowerCase() }));
-};
